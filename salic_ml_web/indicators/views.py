d7305c7723004792ef3125b8761389e3083b21b0
Eduardo Nunes <eduardonunes2525@gmail.com>
2018-08-01 12:06:32 -0300

    Create method show_metrics in view to render page

--- salic_ml_web/indicators/views.py
+++ salic_ml_web/indicators/views.py
@@ -3,8 +3,17 @@ from django.http import HttpResponse
 from salic_db.utils import testConnection
 
 def index(request):
-    a = [1,2,3,4]
-    return render(request, 'show_metrics.html', {'a': a})
+    projects = [
+        {"pronac": "1234", "complexity": 25, "title": "Show do Tiririca", "responsible": "Florentina"},
+        {"pronac": "4321", "complexity": 75, "title": "Show do ex Calipso", "responsible": "Chimbinha"},
+        {"pronac": "1324", "complexity": 95, "title": "Projeto da Cláudia Leitte", "responsible": "Cláudia Leitte"},
+        {"pronac": "1243", "complexity": 15, "title": "Tourada", "responsible": "Ferdinando"},
+        {"pronac": "2143", "complexity": 5, "title": "Projeto modelo", "responsible": "Modelo"},
+    ]
+    return render(request, 'index.html', {'projects': projects})
+
+def show_metrics(request):
+    return render(request, 'show_metrics.html', {})
 
 def dbConnectionTest(request):
     connection_result = testConnection()
