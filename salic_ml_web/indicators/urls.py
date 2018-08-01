c5724dd12b436c5e9cdf840bbdc8df54c2ef23b1
Eduardo Nunes <eduardonunes2525@gmail.com>
2018-08-01 12:06:32 -0300

    Create method show_metrics in view to render page

--- salic_ml_web/indicators/urls.py
+++ salic_ml_web/indicators/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('show', views.show_metrics, name='metrics'),
     path('db_test', views.dbConnectionTest, name='db_test')
 ]
