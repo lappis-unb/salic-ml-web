85cd1824edd275edb26e1e1417c71a87aed15705
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2018-08-01 18:02:43 +0000

    Fix indicators index

--- salic_ml_web/indicators/urls.py
+++ salic_ml_web/indicators/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('db_test', views.db_connection_test, name='db_test'),
-    path('db_query', views.projects_to_analyse, name='make_query')
 ]
