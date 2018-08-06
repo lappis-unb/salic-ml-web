6e1b6df3ac6316b0ae9577130db4e16c59683406
Eduardo Nunes <eduardonunes2525@gmail.com>
2018-08-06 15:58:36 -0300

    Add data post of users and project feedbacks
    Signed-off-by: Eduardo Nunes <eduardonunes2525@gmail.com>
    Co-authored-by: Victor Moura <victor_cmoura@hotmail.com>

--- salic_ml_web/indicators/urls.py
+++ salic_ml_web/indicators/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('db_test', views.db_connection_test, name='db_test'),
     path('db_query', views.projects_to_analyse, name='make_query'),
     path('<int:pronac>', views.show_metrics, name='show_metrics'),
-    path('user_info', views.fetch_user_data, name='user_info')
+    path('user_info', views.fetch_user_data, name='user_info'),
+    path('metrics_feedback', views.post_metrics_feedback, name='metrics_feedback_info')
 ]
