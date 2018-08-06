ec7e7dc66043f3b4a0529be2899423c11ba95e3b
Victor Moura <victor_cmoura@hotmail.com>
2018-08-06 09:37:10 -0300

    Fetching user info params from project page

--- salic_ml_web/indicators/models.py
+++ salic_ml_web/indicators/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Entity(models.Model):
-    pronac = models.CharField(max_length=10)
+    pronac = models.IntegerField(default=0)
     name = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
