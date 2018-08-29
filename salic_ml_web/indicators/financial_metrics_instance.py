88ac22d0d1e060820f6ed451a988abe7b567f74b
Victor Moura <victor_cmoura@hotmail.com>
2018-08-29 10:13:34 -0300

    Rearranging indicators urls and financial_metrics instance

---
+++ salic_ml_web/indicators/financial_metrics_instance.py
@@ -0,0 +1,7 @@
+from core.finance.financial_metrics import FinancialMetrics
+
+# Instanciating FinancialMetrics global module
+financial_metrics = FinancialMetrics()
+
+# Uncomment line below when deploying
+financial_metrics.save()
--- salic_ml_web/indicators/urls.py
+++ salic_ml_web/indicators/urls.py
@@ -12,11 +12,11 @@ indicators_router.register(r'metric_feedbacks', views.MetricFeedbackViewSet)
 indicators_router.register(r'project_feedbacks', views.ProjectFeedbackViewSet)
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    url(r'v2/', include(indicators_router.urls)),
-    path('db_test', views.db_connection_test, name='db_test'),
-    path('db_query', views.projects_to_analyse, name='make_query'),
-    path('user_info', views.fetch_user_data, name='user_info'),
-    path('metrics_feedback', views.post_metrics_feedback, name='metrics_feedback_info'),
-    path('<str:pronac>', views.show_metrics, name='show_metrics'),
+    # path('', views.index, name='index'),
+    url(r'', include(indicators_router.urls)),
+    # path('db_test', views.db_connection_test, name='db_test'),
+    # path('db_query', views.projects_to_analyse, name='make_query'),
+    # path('user_info', views.fetch_user_data, name='user_info'),
+    # path('metrics_feedback', views.post_metrics_feedback, name='metrics_feedback_info'),
+    # path('<str:pronac>', views.show_metrics, name='show_metrics'),
 ]
