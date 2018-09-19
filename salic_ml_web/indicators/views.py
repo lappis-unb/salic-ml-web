200a53ef396b4fc8861afa2a1c9c47fad34dae5e
Eduardo Nunes <eduardonunes2525@gmail.com>
2018-09-19 16:42:31 -0300

    Change page of success message

--- salic_ml_web/indicators/views.py
+++ salic_ml_web/indicators/views.py
@@ -681,25 +681,5 @@ def post_metrics_feedback(request):
             except:
                 continue
 
-    # return HttpResponse(status=201)
-    try:
-        projects = projects_to_analyse(request)
-    except:
-        projects = [
-        {"pronac": "90021", "complexity": 25,
-            "project_name": "Indie 2009 - Mostra de Cinema Mundial", "analist": "Florentina"},
-        {"pronac": "153833", "complexity": 75,
-            "project_name": "TRES SOMBREROS DE COPA", "analist": "Chimbinha"},
-        {"pronac": "160443", "complexity": 95,
-            "project_name": "SERGIO REIS – CORAÇÃO ESTRADEIRO", "analist": "Cláudia Leitte"},
-        {"pronac": "118593", "complexity": 15,
-            "project_name": "ÁGUIA  CARNAVAL 2012: TROPICÁLIA! O MOVIMENTO QUE NÃO TERMINOU", "analist": "Ferdinando"},
-        {"pronac": "161533", "complexity": 5,
-            "project_name": "“Livro sobre Serafim Derenzi” (título provisório)", "analist": "Modelo"},
-        {"pronac": "171372", "complexity": 5,
-            "project_name": "“Paisagismo Brasileiro, Roberto Burle Marx e Haruyoshi Ono – 60 anos de história”.", "analist": "Modelo"},
-        {"pronac": "92739", "complexity": 5,
-            "project_name": "Circulação de oficinas e shows - Claudia Cimbleris", "analist": "Modelo"},
-    ]
-    # return redirect(index, submit_success = True)
-    return render(request, 'index.html', {'submit_success': True, 'projects': projects})
+    # return render(request, 'index.html', {'submit_success': True, 'projects': projects})
+    return render(request, 'show_metrics.html', {'submit_success': True, 'user': None, 'project': entity})
