40760fd0e3d3d44d05cc5820d24ecba8d736f8f7
Victor Moura <victor_cmoura@hotmail.com>
2018-10-31 14:54:32 -0300

    Enhance gunicorn worker timeout to 120s

--- salic_ml_web/init_dev.sh
+++ salic_ml_web/init_dev.sh
@@ -7,4 +7,4 @@ echo "** Caching information **"
 sh redis_cache.sh
 echo "** Attempting to start service **"
 # python3 manage.py runserver 0.0.0.0:8080
-gunicorn --env DJANGO_SETTINGS_MODULE=salic_ml_web.settings -b :8080 salic_ml_web.wsgi --reload
+gunicorn --env DJANGO_SETTINGS_MODULE=salic_ml_web.settings -b :8080 salic_ml_web.wsgi --reload --timeout 120
